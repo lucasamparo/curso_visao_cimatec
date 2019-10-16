2e77a6d06e1e2a1cebcf5956020997c2db2eed1a
Lucas Amparo <lucasamparo.ti@gmail.com>
2019-10-16 10:00:37 -0300

    Resolvendo conflitos com CV4

--- aula_4/src/haar_hand.cpp
+++ aula_4/src/haar_hand.cpp
@@ -34,11 +34,11 @@ void detectAndDisplay( Mat frame ) {
   std::vector<Rect> hand;
   Mat frame_gray;
 
-  cvtColor( frame, frame_gray, CV_BGR2GRAY );
+  cvtColor( frame, frame_gray, COLOR_BGR2GRAY );
   equalizeHist( frame_gray, frame_gray );
 
   //-- Detect faces
-  hand_cascade.detectMultiScale( frame_gray, hand, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE, Size(30, 30) );
+  hand_cascade.detectMultiScale( frame_gray, hand, 1.01, 2, 0|CASCADE_SCALE_IMAGE, Size(30, 30) );
 
   for( size_t i = 0; i < hand.size(); i++ ) {
     Point center( hand[i].x + hand[i].width*0.5, hand[i].y + hand[i].height*0.5 );
@@ -46,6 +46,5 @@ void detectAndDisplay( Mat frame ) {
   }
   //-- Show what you got
   imshow( window_name, frame );
-  imwrite("output.png", frame);
 }
 
